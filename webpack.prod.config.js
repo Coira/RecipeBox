8fe54a08bdeb42c0abe8b4e45dfc242b78c03dd9
Coira <coira.ells@gmail.com>
2016-10-24 11:25:27 +0100

    no more hot loader errors in the console

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,9 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/only-dev-server',
-        './src'
+		'./src'
     ],
     module: {
         loaders: [
